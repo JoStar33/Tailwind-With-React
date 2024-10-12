3a3e1176fdfabc8abc3fdc88fa7ec85185e6b876
HoCG <rhwkd5012@naver.com>
2024-10-12 23:28:04 +0900

    design: 기본 스타일 적용 연습

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,29 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 function App() {
   return (
-    <div className="flex items-center justify-center border border-indigo-600">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
+    <div className="flex justify-center items-center h-dvh">
+      <div className="w-56">
+        <div className="flex items-center justify-center border border-indigo-600 w-full">
+          <a
+            className="w-1/2 flex items-center justify-center h-20"
+            href="https://vitejs.dev"
+            target="_blank"
+          >
+            <img src={viteLogo} className="logo select-none" alt="Vite logo" />
+          </a>
+          <a
+            className="w-1/2 flex items-center justify-center h-20"
+            href="https://react.dev"
+            target="_blank"
+          >
+            <img
+              src={reactLogo}
+              className="logo react select-none"
+              alt="React logo"
+            />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }

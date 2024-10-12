d6aa446e937d13e863a0decddb93be5fb6b976ad
HoCG <rhwkd5012@naver.com>
2024-10-12 23:06:29 +0900

    init: 테일윈드 초기세팅

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+function App() {
+  return (
+    <div className="flex items-center justify-center border border-indigo-600">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+  );
+}
+
+export default App;
